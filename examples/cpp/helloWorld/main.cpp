78dee3d08916d38d6c254eda627bb3cbf35e228f
Brian Malloy <malloy@clemson.edu>
2017-08-24 15:17:36 -0400

    new changes

--- examples/cpp/helloWorld/main.cpp
+++ examples/cpp/helloWorld/main.cpp
@@ -1,4 +1,19 @@
 #include <iostream>
+int i = 7;
+
+namespace MySpace {
+  void f(int x) {
+    x = x + 1;
+  }
+  int i = 17;
+}
+
+void f(int x) {
+  x = x + 1;
+}
 
 int main() {
+  int i = 99;
+  f(i);
+  std::cout << "i is: " << i << std::endl;
 }
