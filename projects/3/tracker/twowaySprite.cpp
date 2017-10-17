068954211517e6c6c465fcef9feeb6c4128fcc7f
haritha2112 <hrathin@g.clemson.edu>
2017-10-17 18:32:34 -0400

    Adds new two_way_sprite files and cloud file

---
+++ projects/3/tracker/twowaySprite.cpp
@@ -0,0 +1,76 @@
+#include "twowaySprite.h"
+#include "gamedata.h"
+#include "renderContext.h"
+
+void TwoWaySprite::advanceFrame(Uint32 ticks) {
+	timeSinceLastFrame += ticks;
+	if (timeSinceLastFrame > frameInterval) {
+    currentFrame = (currentFrame+1) % numberOfFrames;
+		timeSinceLastFrame = 0;
+	}
+}
+
+TwoWaySprite::TwoWaySprite( const std::string& name) :
+  Drawable(name, 
+           Vector2f(Gamedata::getInstance().getXmlInt(name+"/startLoc/x"), 
+                    Gamedata::getInstance().getXmlInt(name+"/startLoc/y")), 
+           Vector2f(Gamedata::getInstance().getXmlInt(name+"/speedX"),
+                    Gamedata::getInstance().getXmlInt(name+"/speedY"))
+           ),
+  images( RenderContext::getInstance()->getImages(name) ),
+
+  currentFrame(0),
+  numberOfFrames( Gamedata::getInstance().getXmlInt(name+"/frames") ),
+  frameInterval( Gamedata::getInstance().getXmlInt(name+"/frameInterval")),
+  timeSinceLastFrame( 0 ),
+  worldWidth(Gamedata::getInstance().getXmlInt("world/width")),
+  worldHeight(Gamedata::getInstance().getXmlInt("world/height")),
+  frameWidth(images[0]->getWidth()),
+  frameHeight(images[0]->getHeight()),
+  timeToFlip(false)
+{ }
+
+TwoWaySprite::TwoWaySprite(const TwoWaySprite& s) :
+  Drawable(s), 
+  images(s.images),
+  currentFrame(s.currentFrame),
+  numberOfFrames( s.numberOfFrames ),
+  frameInterval( s.frameInterval ),
+  timeSinceLastFrame( s.timeSinceLastFrame ),
+  worldWidth( s.worldWidth ),
+  worldHeight( s.worldHeight ),
+  frameWidth( s.frameWidth ),
+  frameHeight( s.frameHeight ),
+  timeToFlip(s.timeToFlip)
+  { }
+
+void TwoWaySprite::draw() const { 
+  images[currentFrame]->draw(getX(), getY(),timeToFlip);
+}
+
+void TwoWaySprite::update(Uint32 ticks) { 
+  advanceFrame(ticks);
+
+  Vector2f incr = getVelocity() * static_cast<float>(ticks) * 0.001;
+  setPosition(getPosition() + incr);
+
+  if ( getY() < 0) {
+    setVelocityY( fabs( getVelocityY() ) );
+  }
+  if ( getY() > worldHeight-frameHeight) {
+    setVelocityY( -fabs( getVelocityY() ) );
+  }
+
+  if ( getX() < 0) {
+    setVelocityX( fabs( getVelocityX() ) );
+    timeToFlip=false;
+   // std::cout << "should flip back" << std::endl;
+  }
+  if ( getX() > worldWidth-frameWidth) {
+    setVelocityX( -fabs( getVelocityX() ) );
+    timeToFlip=true;
+     // std::cout << "should flip" << std::endl;
+
+  }  
+
+}
